4741363009dd768dc0022708010ff4340333bacb
William Mantegna <wmantegna@gmail.com>
2017-01-06 13:30:38 -0500

    added update password feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+
+  # Custom Update Password
+  resource :user, only: [:edit] do
+    collection do
+      patch 'update_password'
+    end
+  end
+
+
   resources :products
   root 'products#index'
 end
